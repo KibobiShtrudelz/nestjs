f945ed4e51be2fef3e8db97b6a99319568968280
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2023-09-05 10:31:03 +0300

    init new app

---
+++ src/reports/reports.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Report } from './entities/report.entity';
+import { ReportsService } from './reports.service';
+import { ReportsController } from './reports.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Report])], // Това създава "repository"-то за нас
+  controllers: [ReportsController],
+  providers: [ReportsService],
+})
+export class ReportsModule {}
