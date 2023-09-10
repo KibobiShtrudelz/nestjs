937ddfee8f535d78e4c3491ccdc5aa68d83d9660
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2023-09-11 01:10:35 +0300

    still unit testing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -7,10 +7,11 @@ import { UsersService } from './users.service'
 
 describe('AuthService', () => {
   let service: AuthService
+  let fakeUsersService: Partial<UsersService>
 
   beforeEach(async () => {
     // Create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User)
@@ -42,5 +43,32 @@ describe('AuthService', () => {
 
     expect(salt).toBeDefined()
     expect(hash).toBeDefined()
+  })
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    // Така презаписваме "find" метода от fakeUsersService, за да можем
+    // да използваме "find", както ни е необходим тук, т.е. да връща обект с потребител
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'ko@staa.men', password: '1' } as User])
+    await expect(service.signUp('ko@staa.men', 'staa')).rejects.toThrow(BadRequestException)
+
+    // Когато директно връщаме нещо в колбека не бива да ползваме "done()", който идва като аргумент в колбека.
+    // TS изпръцква с async колбеците и затова или го правим така, както е написано или в къдрави скоби ползваме done(),
+    // който идва като аргумент от асинхронният колбек
+  })
+
+  it('throws if signIn is called with an unused email', async () => {
+    await expect(service.signIn('asdflkj@asdlfkj.com', 'passdflkj')).rejects.toThrow(
+      BadRequestException
+    )
+  })
+
+  it('throws if an invalid password is provided', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ email: 'asdf@asdf.com', password: 'laskdjf' } as User])
+
+    await expect(service.signIn('laskdjf@alskdfj.com', 'passowrd')).rejects.toThrow(
+      BadRequestException
+    )
   })
 })
