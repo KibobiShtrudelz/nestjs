bde4dfd5a7db3b5d34b2698e5e08d41f1ec7c4e0
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2023-09-17 11:03:32 +0300

    adding relations

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,7 +2,9 @@ import { Post, Body, UseGuards, Controller } from '@nestjs/common'
 
 import { AuthGuard } from '../guards/auth.guard'
 import { ReportsService } from './reports.service'
+import { User } from 'src/users/entities/user.entity'
 import { CreateReportDto } from './dtos/ceate-report-dto'
+import { CurrentUser } from 'src/users/decorators/current-user.decorator'
 
 @Controller('reports')
 export class ReportsController {
@@ -10,7 +12,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user)
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,14 +4,17 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 
 import { Report } from './entities/report.entity'
+import { User } from 'src/users/entities/user.entity'
 import { CreateReportDto } from './dtos/ceate-report-dto'
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private readonly reportsRepository: Repository<Report>) {}
 
-  create(reportDto: CreateReportDto) {
+  create(reportDto: CreateReportDto, user: User) {
     const report = this.reportsRepository.create(reportDto)
+    report.user = user // Зад колисите репозиторито ще закачи към "report"-а САМО "userId"-то към "report" обекта
+
     return this.reportsRepository.save(report)
   }
 }
