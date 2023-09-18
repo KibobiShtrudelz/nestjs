ef29851460799a7b8a5e0432e069829159d8465a
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2023-09-18 23:40:37 +0300

    fixing a type definition error

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -2,7 +2,20 @@ import { Injectable, NestMiddleware } from '@nestjs/common'
 
 import { Request, Response, NextFunction } from 'express'
 
+import { User } from '../entities/user.entity'
 import { UsersService } from '../users.service'
+
+// Обновяваме "Request" интерфейсa на Express, защото иначе TS се оплаква, че
+// request няма property currentUser (ред 30)
+declare global {
+  namespace Express {
+    // => "Намери ми 'Express' библиотеката"
+    interface Request {
+      // => "Намери ми 'Request' интерфейса в 'Express' библиотеката"
+      currentUser?: User // => "Добави ново property към 'Request' интерфейса"
+    }
+  }
+} // Така добавяме ново property към вече съществуващ интерфейс
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -14,7 +27,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
     if (userId) {
       const user = await this.usersService.findOne(userId)
 
-      // @ts-ignore
       request.currentUser = user
     }
 
