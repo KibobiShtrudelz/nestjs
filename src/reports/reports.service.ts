a342cdc3e9330e5ecbab2f2b80efb575fbba47e8
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2023-09-19 17:58:47 +0300

    add query builder to the estimate end-point

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -6,11 +6,27 @@ import { Repository } from 'typeorm'
 
 import { Report } from './entities/report.entity'
 import { User } from 'src/users/entities/user.entity'
+import { GetEstimateDto } from './dtos/get-estimate.dto'
 import { CreateReportDto } from './dtos/ceate-report-dto'
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private readonly reportsRepository: Repository<Report>) {}
+
+  createEstimate({ lng, lat, make, year, model, mileage }: GetEstimateDto) {
+    return this.reportsRepository
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne()
+  }
 
   create(reportDto: CreateReportDto, user: User) {
     const report = this.reportsRepository.create(reportDto)
